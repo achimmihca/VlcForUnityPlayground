112bc0617df3f45c6881af6f7e32a29a70a66704
Andreas Stange <anst.dev@gmail.com>
2026-05-12 16:49:09 +0200

    add ExamplePlayModeTest

---
+++ Assets/PlayModeTests/ExamplePlayModeTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class ExamplePlayModeTest
+{
+    [UnityTest]
+    public IEnumerator ShouldSucceed()
+    {
+        // VLC for Unity removes these log lines
+        // after the test finished (see VLCNativePluginProcessor.PluginErrorCleaner.ClearPluginErrors)
+        Debug.Log("Test started");
+        Debug.Log("Test done");
+        yield return null;
+    }
+}
